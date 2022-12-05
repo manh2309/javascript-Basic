d02363c994c06495b3e27691363cfa13f77733c4
manh2309 <manh230920011@gmail.com>
2022-12-05 21:22:37 +0700

    Variable, Constants(Data type)

---
+++ bai3/bai3.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 6.6;
+let x = 5;
+let y = 5;
+let z = 6;
+// Const là hàng số , mà giá trị hằng số k bao giờ đổi
+const d = 5;
+
+// Biến boolean
+console.log("Hello World from JavaScript: ", (x == y) + " " + (z == z));
+
+console.log("Hello World from JavaScript: ", d);
+// Khai báo nhiều biến cùng lúc
+let q = 5,
+  w = 6,
+  e = 7;
+// In nhiều biến cùng lúc
+console.log("In string: ", " q = ", q, " w = ", w, " e = ", e);
